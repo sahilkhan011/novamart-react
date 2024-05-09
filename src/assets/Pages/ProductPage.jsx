a58cefb95d5954c0073adc370f172e9394e2ffb2
Sahil Khan <sahilkhan468786@gmail.com>
2024-05-09 13:17:06 +0530

    product design

--- src/assets/Pages/ProductPage.jsx
+++ src/assets/Pages/ProductPage.jsx
@@ -1,16 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./productpage.module.css";
 import { FaChevronLeft, FaChevronRight, FaShoppingCart } from "react-icons/fa";
+import newTagImage from "./../new_tag.gif";
 
 import Responsive from "../../Slick/Responsive";
 
 const ProductPage = () => {
-  const [images, setImages] = useState([
-    "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/3396ee3c-08cc-4ada-baa9-655af12e3120/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/e44d151a-e27a-4f7b-8650-68bc2e8cd37e/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/44fc74b6-0553-4eef-a0cc-db4f815c9450/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-    "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d3eb254d-0901-4158-956a-4610180545e5/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
-  ]);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    /*const fetchData = async () => {
+      try {
+        const subcategoriesData = await getSubCategories(catid);
+        console.log(subcategoriesData);
+        setSubCategories(subcategoriesData);
+        // Do something with the categoriesData, such as setting it in state
+      } catch (error) {
+        // Handle the error
+      }
+    };
+
+    fetchData();
+    */
+    setImages([
+      "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/3396ee3c-08cc-4ada-baa9-655af12e3120/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+      "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/e44d151a-e27a-4f7b-8650-68bc2e8cd37e/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+      "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/44fc74b6-0553-4eef-a0cc-db4f815c9450/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+      "https://static.nike.com/a/images/f_auto,b_rgb:f5f5f5,w_440/d3eb254d-0901-4158-956a-4610180545e5/scarpa-da-running-su-strada-invincible-3-xk5gLh.png",
+    ]);
+  }, []);
 
   const [activeIndex, setActiveIndex] = useState(0);
   const [amount, setAmount] = useState(1);
@@ -78,12 +96,15 @@ const ProductPage = () => {
           </div>
           <div className="col-md-8">
             <div className={`${styles.productDetails}`}>
-              <h2 className={`${styles.productName}`}>Nike Invincible 3</h2>
+              <div className="d-flex align-items-center">
+                <h2 className={`${styles.productName}`}>Nike Invincible </h2>
+                <img width={50} src={newTagImage} alt="" />
+              </div>
               <div
                 className={`${styles.productPrice} d-flex align-items-baseline`}
               >
-                <h3 className="d-flex">$80</h3>
-                <del className={`${styles.oldPrice}`}>$100</del>
+                <h3 className="d-flex">&#8377;80</h3>
+                <del className={`${styles.oldPrice}`}>&#8377;100</del>
                 <span className={`${styles.productAvailability}`}>
                   In Stock
                 </span>
@@ -100,14 +121,14 @@ const ProductPage = () => {
             <div className="d-flex align-items-center gap-3">
               <div className="d-flex align-items-center gap-2">
                 <button
-                  className="btn btn-outline-secondary"
+                  className={`${styles.amountBtn} `}
                   onClick={handleDecreaseAmount}
                 >
                   -
                 </button>
                 <span className="rounded">{amount}</span>
                 <button
-                  className="btn btn-outline-secondary"
+                  className={`${styles.amountBtn} `}
                   onClick={handleIncreaseAmount}
                 >
                   +
@@ -124,6 +145,8 @@ const ProductPage = () => {
           </div>
         </div>
         <h1>Related Products</h1>
+        <Responsive />
+        <h1>Trending Products</h1>
         <Responsive />
       </div>
     </div>
